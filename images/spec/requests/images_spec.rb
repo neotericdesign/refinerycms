d811860b4844dc14f6585d809e1b3c695505a372
Joe Sak <joe@joesak.com>
2011-10-21 16:52:15 -0500

    WIP: putting tags on images when created... the controller logic is blocking this

--- images/spec/requests/images_spec.rb
+++ images/spec/requests/images_spec.rb
@@ -1,24 +1,30 @@
 require 'spec_helper'
 
-describe "Images" do
+describe "Images", :focus => true do
   describe "tag lists" do
-    before(:each) do
-      @image1 = Factory(:image, :tag_list => 'Events, Fun')
-      @image2 = Factory(:image, :tag_list => 'Other Tag, Something else')
-
-      refinery = Factory(:refinery_user)
+    before(:each) do    
+      @refinery ||= Factory(:refinery_user)
       visit admin_root_path
-      fill_in "Login", :with => refinery.username
-      fill_in "Password", :with => refinery.password
+      fill_in "Login", :with => @refinery.username
+      fill_in "Password", :with => @refinery.password
       click_button "Sign in"
     end
     
-    it "are navigated via the admin sidebar", :focus => true do
+    it "are navigated via the admin sidebar" do
+      Factory(:image, :tag_list => 'Events, Fun')
       visit admin_images_path
       click_link "Fun"
       
       current_path.should eq(tagged_admin_images_path(ActsAsTaggableOn::Tag.find_by_name 'Fun'))
       page.should have_content('Images tagged "Fun"')
+    end
+    
+    it "are created in the new image form" do
+      visit new_admin_image_path
+      attach_file 'image_image', "#{Rails.root}/images/spec/uploads/beach.jpeg"
+      fill_in "Tags", :with => "beach, fun, sun"
+      click_button "Save"
+      page.should have_link('sun')
     end
   end
 end
